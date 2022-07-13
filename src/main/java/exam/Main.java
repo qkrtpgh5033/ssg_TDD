4c83c06483580866e4883cff2d09893aeeea1100
qkrtpgh5033 <qkrtpgh5033@gmail.com>
2022-07-13 13:15:04 +0900

    세팅

---
+++ src/main/java/exam/Main.java
@@ -0,0 +1,9 @@
+package exam;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        new App().run();
+    }
+}
---
+++ src/test/java/exam/AppTest.java
@@ -0,0 +1,7 @@
+package exam;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AppTest {
+
+}
