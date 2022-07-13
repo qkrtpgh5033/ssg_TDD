1699583bcf63e731af3b19c389d08e8d8e91232c
qkrtpgh5033 <qkrtpgh5033@gmail.com>
2022-07-13 14:05:38 +0900

    리다이렉팅 TestRunner 도입

---
+++ src/test/java/com/ll/exam/AppTest.java
@@ -0,0 +1,66 @@
+package com.ll.exam;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.util.Scanner;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AppTest {
+    @Test
+    public void 등록을_하면_명언과_작가를_물어본다() {
+        String rs = AppTestRunner.run("""
+                등록
+                나의 죽음을 적들에게 알리지 말라
+                이순신
+                종료
+                """);
+
+        assertTrue(rs.contains("명언 : "));
+        assertTrue(rs.contains("작가 : "));
+    }
+
+    @Test
+    public void 프로그램_시작시_타이틀_출력_그리고_종료() {
+        String rs = AppTestRunner.run("""
+                종료
+                """);
+
+        assertTrue(rs.contains("== 명언 SSG =="));
+        assertTrue(rs.contains("명령)"));
+    }
+
+    @Test
+    public void 테스트() {
+        assertTrue(1 == 1);
+        assertEquals(1, 1);
+    }
+
+    @Test
+    public void 출력을_모니터에_하지_않고_문자열로_얻기() {
+        ByteArrayOutputStream output = TestUtil.setOutToByteArray();
+        System.out.print("안녕");
+        String rs = output.toString();
+        TestUtil.clearSetOutToByteArray(output);
+    }
+
+    @Test
+    public void 스캐너에_키보드가_아닌_문자열을_입력으로_설정() {
+        Scanner sc = TestUtil.genScanner("안녕");
+
+        String cmd = sc.nextLine().trim();
+        assertEquals("안녕", cmd);
+    }
+
+    @Test
+    public void 문자열을_파일에_저장() {
+        Util.file.mkdir("test_data");
+        Util.file.saveToFile("test_data/1.txt", "안녕");
+
+        String body = Util.file.readFromFile("test_data/1.txt");
+
+        assertEquals("안녕", body);
+    }
+}
